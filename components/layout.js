d9a5731f2d162e4ebbabd7806de9430042245211
Alex Zordel <89091166+alexzordel@users.noreply.github.com>
2023-05-03 13:04:48 -0700

    title fix

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
 const name = 'Massage at Home';
-export const siteTitle = 'Muew Muew site title';
+export const siteTitle = 'Tristate Massage';
 
 export default function Layout({ children, home }) {
 
@@ -28,12 +28,12 @@ export default function Layout({ children, home }) {
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        <meta
+        {/* <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        /> */}
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
