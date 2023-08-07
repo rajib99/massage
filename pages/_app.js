83ddc7a7cf86688e1bc79b934850449e63d95081
Alex Zordel <89091166+alexzordel@users.noreply.github.com>
2023-08-07 15:08:02 -0700

    fix ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/global.css';
+import Script from "next/script"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-WEX6FW6EN4"/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WEX6FW6EN4', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
+    <Component {...pageProps} />;
+    </>
+  ) 
 }
