2cb0e66bd8461ee734ba6bd5c0511725ef4195f7
Alex Zordel <89091166+alexzordel@users.noreply.github.com>
2024-02-24 14:12:43 -0800

    url change

--- components/config.js
+++ components/config.js
@@ -1,6 +1,8 @@
 // config.js
 const LOCAL_URL = 'http://localhost:3005/';
-const REMOTE_URL = 'https://app.tristatemassage.com/';
-const CURRENT_URL = LOCAL_URL;
+// const REMOTE_URL = 'https://app.tristatemassage.com/';
+const REMOTE_URL = 'https://app.spagram.com/';
+
+const CURRENT_URL = REMOTE_URL;
 
 export { LOCAL_URL, REMOTE_URL, CURRENT_URL };
