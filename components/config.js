5fa401d905baf902757a059647a05ff28c3698da
Alex Zordel <89091166+alexzordel@users.noreply.github.com>
2024-04-01 00:09:22 -0700

    URL Change

--- components/config.js
+++ components/config.js
@@ -1,8 +1,9 @@
 // config.js
 const LOCAL_URL = 'http://localhost:3005/';
-const REMOTE_URL = 'https://tristatemassage.com/';
+const REMOTE_URL = 'https://spagram.com/';
+const REMOTE_URL_OLD = 'https://tristatemassage.com/';
 // const REMOTE_URL = 'https://app.spagram.com/';
 
-const CURRENT_URL = LOCAL_URL;
+const CURRENT_URL = REMOTE_URL;
 
 export { LOCAL_URL, REMOTE_URL, CURRENT_URL };
